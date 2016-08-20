41bd5d18146c6db2122d5bebbac91770be397144
Carl Lemaire <c.lemaire@live.ca>
2016-08-20 01:48:30 -0400

    Courant selon sa direction

--- Assets/Script/courantAirChaud.cs
+++ Assets/Script/courantAirChaud.cs
@@ -1,18 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum WindAxis { x, y, z }
+
 public class courantAirChaud : MonoBehaviour {
 
     public GameObject avion;
-
-    public Vector3 appliedVelocity;
+    public Vector3 velocityVector;
+    public float magnitude = 1.0f;
+    public bool useVelocityVector = false;
+    public WindAxis windAxis;
 
     private PlayerController playerController;
+    private Vector3 objectAxis;
+    private Vector3 actualVelocityVector;
 
 	// Use this for initialization
 	void Start () {
         Debug.Log("bob");
         playerController = avion.GetComponent<PlayerController>();
+
+        if (useVelocityVector)
+        {
+            actualVelocityVector = velocityVector;
+        }
+        else
+        {
+            if (windAxis == WindAxis.x)
+                objectAxis = gameObject.transform.right;
+            else if (windAxis == WindAxis.y)
+                objectAxis = gameObject.transform.up;
+            else
+                objectAxis = gameObject.transform.forward;
+
+            actualVelocityVector = magnitude * objectAxis;
+        }
     }
 	
 	// Update is called once per frame
@@ -23,12 +45,12 @@ public class courantAirChaud : MonoBehaviour {
     void OnTriggerEnter ()
     {
         Debug.Log("enter");
-        playerController.externalVelocity += appliedVelocity;
+        playerController.externalVelocity += actualVelocityVector;
     }
 
     void OnTriggerExit()
     {
         Debug.Log("leave");
-        playerController.externalVelocity -= appliedVelocity;
+        playerController.externalVelocity -= actualVelocityVector;
     }
 }
