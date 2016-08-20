39493430c0ff4331cdaeddfdb4a439e6fc299181
Catalin <catalinp86@gmail.com>
2016-08-19 23:36:02 -0400

    Player controller working

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, yMin, yMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+    public Boundary boundary;
+    public float speed = 10.0f;
+    public Vector3 tilt;
+
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float moveHorizontal = -Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, -1.0f);
+        rb.velocity = speed * movement;
+
+        rb.position = new Vector3
+        (
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+            Mathf.Clamp(rb.position.y, boundary.yMin, boundary.yMax),
+            rb.position.z
+        );
+
+        rb.rotation = Quaternion.Euler(rb.velocity.y * + tilt.x, rb.velocity.x * -tilt.y, rb.velocity.x * -tilt.z);
+    }
+}
