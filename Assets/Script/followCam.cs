9be5a6b7a21a8756caf2100dc8a4f4693b0087fb
Carl Lemaire <c.lemaire@live.ca>
2016-08-20 02:30:51 -0400

    Smooth Follow Cam

--- Assets/Script/followCam.cs
+++ Assets/Script/followCam.cs
@@ -8,14 +8,20 @@ public class followCam : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
         var targetPos = target.transform.position;
         targetPos += offset;
         gameObject.transform.position = targetPos;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        var targetPos = target.transform.position;
+        targetPos += offset;
+
+        var camPos = gameObject.transform.position;
+        camPos += (targetPos - camPos) * 4.0f * Time.deltaTime;
+        gameObject.transform.position = camPos;
         //Debug.Log(gameObject.transform.position);
+        //gameObject.transform.position = targetPos;
     }
 }
