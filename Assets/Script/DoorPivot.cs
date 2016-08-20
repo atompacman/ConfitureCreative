9c33dd36916337587412ca18bb57d29e79c25a7e
mannad <marc-antoine.nadeau@usherbrooke.ca>
2016-08-20 03:06:36 -0400

    door and thrower

---
+++ Assets/Script/DoorPivot.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorPivotScript : MonoBehaviour {
+    
+    [Range(0,100)]
+    public float doorSpeed = 1.0f;
+
+    bool animate = false;
+    bool Opened = false;
+
+    public void Open()
+    {
+        animate = true;
+    }
+
+	// Use this for initialization
+	void Start () {
+        gameObject.transform.rotation = Quaternion.identity;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //animate = true;
+        }
+        if( animate && !Opened)
+        {
+            OpenDoor();
+        }
+    }
+
+    void OpenDoor()
+    {
+        if( gameObject.transform.rotation.eulerAngles.y < 90.0f || gameObject.transform.rotation.eulerAngles.y > 180.0f )
+        {
+            gameObject.transform.rotation = Quaternion.AngleAxis(Time.deltaTime * doorSpeed, new Vector3(0, 1, 0)) * gameObject.transform.rotation;
+        }
+        else if (gameObject.transform.rotation.y >= 90.0f )
+        {
+            Opened = false;
+        }
+    }
+}
