d3997fb2c24c71d3d0c91a4a1fd08e11fb5499a4
Carl Lemaire <c.lemaire@live.ca>
2016-08-20 00:11:58 -0400

    Basic movement; Test walls

---
+++ Assets/Script/followCam.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class followCam : MonoBehaviour {
+
+    public GameObject target;
+    public Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var targetPos = target.transform.position;
+        targetPos += offset;
+        gameObject.transform.position = targetPos;
+        //Debug.Log(gameObject.transform.position);
+    }
+}
